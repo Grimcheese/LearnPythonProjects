bf6049568b94785057afa228cc51315b70898c73
AlbyChawula <albychawula@gmail.com>
2022-07-24 13:13:38 +0800

    Finished Number Guessing Game

--- NumberGuessing/main.py
+++ NumberGuessing/main.py
@@ -10,6 +10,8 @@ class NumberGenerator:
     def __init__(self):
         self.randmNumber = None
         self.hint = None
+        self.score = 10
+        self.loseVal = False
 
 
     def generate_number(self):
@@ -21,28 +23,49 @@ class NumberGenerator:
         winVal = False
         if userNum == self.randomNumber:
             self.hint = "You win!"
+            self.modify_score(10)
             winVal = True
+        elif userNum < NumberGenerator.lowerBound or userNum > NumberGenerator.upperBound:
+            self.hint = "Outside of range!"
+            self.modify_score(-5)
         elif userNum > self.randomNumber:
             self.hint = "Too high!"
+            self.modify_score(-2)
         elif userNum < self.randomNumber:
             self.hint = "Too low!"
+            self.modify_score(-2)
         
         return winVal
 
-            
+    def modify_score(self, val):
+        if type(val) == int:
+            self.score = self.score + val
+            if self.score <= 0:
+                self.hint = "You lose"
+                self.loseVal = True
+        else:
+            print("Not a valid modifier")
 
-
-# Allow user to guess
-def get_user_guess(prompt):
-    user_number = input(prompt + ": ")
-    return int(user_number)
+    @classmethod
+    # Allow user to guess and validate input
+    def get_user_guess(cls, prompt):
+        
+        while True:
+            try:
+                user_number = int(input(prompt + ": "))
+            except ValueError:
+                print("Input not a valid integer")
+                continue
+            else:
+                return int(user_number)
 
 NumGen = NumberGenerator()
 number = NumGen.generate_number()
 print(number)
 
 winVal = False
-while winVal == False:
-    user_guess = get_user_guess("Please guess a number between " + str(NumberGenerator.lowerBound) + " and " + str(NumberGenerator.upperBound))
+while winVal == False and NumGen.loseVal == False:
+    user_guess = NumberGenerator.get_user_guess("Please guess a number between " + str(NumberGenerator.lowerBound) + " and " + str(NumberGenerator.upperBound))
     winVal = NumGen.compare_numbers(user_guess)
     print(NumGen.hint)
+    print("Current score: " + str(NumGen.score))
