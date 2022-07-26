07e2386a1ab35fd084cef9f43095380e2ac072de
AlbyChawula <albychawula@gmail.com>
2022-07-26 19:06:28 +0800

    Implementation complete. while loop for binary search is clunky but it works

--- BinarySearch/main.py
+++ BinarySearch/main.py
@@ -11,10 +11,45 @@ def make_list():
         del list[-1]
     return list
 
+# Binary search algo
+
+def binary_search(list, searchVal):
+    lower = 0
+    upper = len(list) - 1
+    
+    searching = True
+    index = -1
+    while searching:
+        mid = upper - (upper - lower) // 2
+        if mid <= lower or mid >= upper: #finished search without finding value
+            
+            searching = False
+        print("Searched cell: " + str(mid))
+        if searchVal ==  list[mid]:
+            #searchVal must equal list[mid]
+            print("Found!")
+            index = mid
+            searching = False
+        elif searchVal > list[mid]:
+            lower = mid + 1
+        elif searchVal < list[mid]:
+            upper = mid - 1
+        
+    return index
+
+
+
 #def binary_search(list, searchkey):
 
 
 newlist = make_list()
 for i in newlist:
     print(i)
+
+searchVal = int(input("Enter number to search for: "))
+foundIndex = binary_search(newlist, searchVal)
+if foundIndex > -1:
+    print("I found " + str(searchVal) + " at index: " + str(foundIndex))
+else:
+    print("Value not found")
 
