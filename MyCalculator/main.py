fb38643a933a5d10eb137b5bc981564936a7e3f6
AlbyChawula <albychawula@gmail.com>
2022-08-01 11:17:02 +0800

    4 basic buttons added

--- MyCalculator/main.py
+++ MyCalculator/main.py
@@ -4,8 +4,28 @@ import tkinter as tk
 
 main_window = tk.Tk()
 
-text = tk.Label(text="My Calculator")
-user_input_number = tk.Entry()
-user_input_number.pack()
+#text = tk.Label(text="My Calculator", foreground = "white", background = "black")
+#text.pack()
+
+top_frame = tk.Frame()
+button_frame = tk.Frame(width = 6)
+
+user_entry = tk.Entry(master = top_frame, bg = "white", fg = "black")
+user_entry.pack()
+
+label2 = tk.Label(master = button_frame, text = "Base")
+label2.pack(fill = tk.X)
+
+plus_button = tk.Button(master = button_frame, text = "+")
+plus_button.pack(fill = tk.X)
+minus_button = tk.Button(master = button_frame, text = "-")
+minus_button.pack(fill = tk.X)
+times_button = tk.Button(master = button_frame, text = "X")
+times_button.pack(fill = tk.X)
+divide_button = tk.Button(master = button_frame, text = "/")
+divide_button.pack(fill = tk.X)
+
+top_frame.pack(side = tk.TOP)
+button_frame.pack(side = tk.RIGHT)
 
 main_window.mainloop()
