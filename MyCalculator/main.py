b170e7b4725aa120901aa59ab37eef03148d3702
AlbyChawula <albychawula@gmail.com>
2022-08-02 15:57:24 +0800

    validating input to main entry box

--- MyCalculator/main.py
+++ MyCalculator/main.py
@@ -2,21 +2,39 @@
 
 import tkinter as tk
 
+def entry_input_callback(input):
+    if input.isdigit():
+        return True
+    elif input == "":
+        return True
+    else:
+        return False
+
+#adds two integer values
+def add_method(value1, value2):
+    sum = value1 + value2
+    user_entry.delete(0, tk.END)
+    user_entry.insert(0, str(sum))
+    
+    print(sum)
+    return sum
+
 main_window = tk.Tk()
 
 #text = tk.Label(text="My Calculator", foreground = "white", background = "black")
 #text.pack()
-
+total = 2
+newVal = 4
 top_frame = tk.Frame()
 button_frame = tk.Frame(width = 6)
 
-user_entry = tk.Entry(master = top_frame, bg = "white", fg = "black")
+user_entry = tk.Entry(master = top_frame, bg = "white", fg = "black", justify = "right")
 user_entry.pack()
 
 label2 = tk.Label(master = button_frame, text = "Base")
 label2.pack(fill = tk.X)
 
-plus_button = tk.Button(master = button_frame, text = "+")
+plus_button = tk.Button(master = button_frame, text = "+", command = add_method(total, newVal))
 plus_button.pack(fill = tk.X)
 minus_button = tk.Button(master = button_frame, text = "-")
 minus_button.pack(fill = tk.X)
@@ -28,4 +46,11 @@ divide_button.pack(fill = tk.X)
 top_frame.pack(side = tk.TOP)
 button_frame.pack(side = tk.RIGHT)
 
+reg = main_window.register(entry_input_callback)
+user_entry.config(validate = "key", validatecommand = (reg, '%P'))
+
+
+user_entry.focus()
+
 main_window.mainloop()
+
