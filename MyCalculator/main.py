3433c0a87b4ae59a02638a0237ae5b6df59456c0
Grimcheese <albychawula@gmail.com>
2022-08-06 14:55:38 +0800

    Created MainApp class which creates and stores the widgets used by the app. Makes it easier for using and modifying the widgets later on without having too many global variables

--- MyCalculator/main.py
+++ MyCalculator/main.py
@@ -7,6 +7,52 @@ class calculator:
         self.runningTotal = 0
         self.currentVal = 0
 
+class MainApp:
+    def __init__(self, parent, calcValues):
+        self.top_frame = tk.Frame(master = parent)
+        self.button_frame = tk.Frame(master = parent, width = 6)
+
+        self.user_entry = tk.Entry(master = self.top_frame, bg = "white", fg = "black", justify = "right")
+        self.user_entry.pack()
+
+        self.label2 = tk.Label(master = self.button_frame, text = "Base")
+        self.label2.pack(fill = tk.X)
+
+        self.plus_button = tk.Button(master = self.button_frame, text = "+", command=lambda: self.add_method(calcValues))
+        self.plus_button.pack(fill = tk.X)
+        self.minus_button = tk.Button(master = self.button_frame, text = "-")
+        self.minus_button.pack(fill = tk.X)
+        self.times_button = tk.Button(master = self.button_frame, text = "X")
+        self.times_button.pack(fill = tk.X)
+        self.divide_button = tk.Button(master = self.button_frame, text = "/")
+        self.divide_button.pack(fill = tk.X)
+        #equals_button = tk.Button(master = button_frame, text = "=", command = lambda: equals_method(total, curentVal))
+
+        self.top_frame.pack(side = tk.TOP)
+        self.button_frame.pack(side = tk.RIGHT)
+
+        reg = parent.register(entry_input_callback)
+        self.user_entry.config(validate = "key", validatecommand = (reg, '%P'))
+
+
+
+
+    #adds two integer values
+    def add_method(self, calcValues):
+
+        currentVal = int(self.get_entry_value())   
+        calcValues.runningTotal = calcValues.runningTotal + currentVal
+        print("The current value in entry is: " + str(currentVal))
+        print("The current value in total is: " + str(calcValues.runningTotal))
+        
+        self.user_entry.insert(tk.END, " + ")
+
+    def get_entry_value(self):
+        if not self.user_entry.get() == "":
+            return int(self.user_entry.get())
+        else:
+            return 0
+
 
 def entry_input_callback(input):
     if input.isdigit():
@@ -16,54 +62,17 @@ def entry_input_callback(input):
     else:
         return False
 
-#adds two integer values
-def add_method(calcValues):
 
-    currentVal = int(get_entry_value())   
-    calcValues.runningTotal = calcValues.runningTotal + currentVal
-    print("The current value in entry is: " + str(currentVal))
-    print("The current value in total is: " + str(calcValues.runningTotal))
-    
-    user_entry.insert(0, " + " + str(calcValues.runningTotal))
-
-
-def get_entry_value():
-    if not user_entry.get() == "":
-        return int(user_entry.get())
-    else:
-        return 0
 
 #initialise required settings
 calcValues = calculator()
 main_window = tk.Tk()
 
+application = MainApp(main_window, calcValues)
+
 #text = tk.Label(text="My Calculator", foreground = "white", background = "black")
 #text.pack()
 
-top_frame = tk.Frame()
-button_frame = tk.Frame(width = 6)
-
-user_entry = tk.Entry(master = top_frame, bg = "white", fg = "black", justify = "right")
-user_entry.pack()
-
-label2 = tk.Label(master = button_frame, text = "Base")
-label2.pack(fill = tk.X)
-
-plus_button = tk.Button(master = button_frame, text = "+", command=lambda: add_method(calcValues))
-plus_button.pack(fill = tk.X)
-minus_button = tk.Button(master = button_frame, text = "-")
-minus_button.pack(fill = tk.X)
-times_button = tk.Button(master = button_frame, text = "X")
-times_button.pack(fill = tk.X)
-divide_button = tk.Button(master = button_frame, text = "/")
-divide_button.pack(fill = tk.X)
-#equals_button = tk.Button(master = button_frame, text = "=", command = lambda: equals_method(total, curentVal))
-
-top_frame.pack(side = tk.TOP)
-button_frame.pack(side = tk.RIGHT)
-
-reg = main_window.register(entry_input_callback)
-user_entry.config(validate = "key", validatecommand = (reg, '%P'))
 
 
 main_window.mainloop()
