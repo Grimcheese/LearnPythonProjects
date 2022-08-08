17a14fc46af0c92333fedf2ddcb6fdbcc3346e0c
Grimcheese <albychawula@gmail.com>
2022-08-08 10:42:30 +0800

    Top frame now displays neatly across whole area. Add, equals and clear buttons have functionality now. Currently only supports using one operator

--- MyCalculator/main.py
+++ MyCalculator/main.py
@@ -6,29 +6,70 @@ class calculator:
     def __init__(self):
         self.runningTotal = 0
         self.currentVal = 0
+        self.currentOperator = ""
 
-    def clearVals(self):
+    
+    #adds two integer values
+    def add_method(self, mainAppClass):
+        input = mainAppClass.user_entry.get()
+        if not input == "":
+            self.currentVal = int(input)
+            self.runningTotal = self.runningTotal + self.currentVal
+            self.currentOperator = "+"
+            
+            print("The current value in entry is: " + str(self.currentVal))
+            print("The current value in total is: " + str(self.runningTotal))
+            
+            mainAppClass.current_total_label.config(text = str(self.runningTotal))
+            mainAppClass.current_operator_label.config(text = " + ")
+            mainAppClass.user_entry.delete(0, tk.END)
+        else:
+            print("No valid input")
+
+    def equalsMethod(self, mainApp):
+        input = mainApp.user_entry.get()
+        if not input == "":
+            mainApp.current_total_label.config(text = str(self.runningTotal) + str(self.currentOperator) + input)
+
+            self.currentVal = int(input)
+            self.runningTotal = self.runningTotal + self.currentVal
+
+            self.currentOperator = " = "
+            mainApp.current_operator_label.config(text = self.currentOperator)
+            mainApp.user_entry.delete(0, tk.END)
+            mainApp.user_entry.insert(0, self.runningTotal)
+
+        
+
+    def clearVals(self, mainApp):
         self.runningTotal = 0
+
+        mainApp.current_total_label.config(text = "")
+        mainApp.current_operator_label.config(text = "")
+        mainApp.user_entry.delete(0, tk.END)
+
+
+    
 
 class MainApp:
     def __init__(self, parent, calcValues):
-        self.top_frame = tk.Frame(master = parent)
+        self.top_frame = tk.Frame(master = parent, width = 20)
         self.button_frame = tk.Frame(master = parent, width = 6)
         self.button_frame2 = tk.Frame(master = parent, width = 6) 
         
 
-        self.current_total_label = tk.Label(master = self.top_frame, text = str(calcValues.runningTotal), bg = "white", fg = "black")
+        self.current_total_label = tk.Label(master = self.top_frame, bg = "white", fg = "black", width = 5)
         self.current_total_label.pack(side = tk.LEFT)
 
+        self.current_operator_label = tk.Label(master = self.top_frame, bg = "white", fg = "black", width = 5)
+        self.current_operator_label.pack(side = tk.LEFT)
+
         reg = parent.register(self.entry_input_callback)
-        self.user_entry = tk.Entry(master = self.top_frame, validate = "key", validatecommand = (reg, '%P'), bg = "white", fg = "black", justify = "right")
-        self.user_entry.pack(side = tk.LEFT)
+        self.user_entry = tk.Entry(master = self.top_frame, validate = "key", validatecommand = (reg, '%P'), relief = tk.FLAT, bg = "white", fg = "black", justify = "right", width = 10)
+        self.user_entry.pack(side = tk.RIGHT)
         self.user_entry.focus_set()
 
-        self.label2 = tk.Label(master = self.button_frame, text = "Base")
-        self.label2.pack(fill = tk.X)
-
-        self.plus_button = tk.Button(master = self.button_frame, text = "+", command=lambda: self.add_method(calcValues))
+        self.plus_button = tk.Button(master = self.button_frame, text = "+", command=lambda: calcValues.add_method(self))
         self.plus_button.pack(fill = tk.X)
         self.minus_button = tk.Button(master = self.button_frame, text = "-")
         self.minus_button.pack(fill = tk.X)
@@ -36,10 +77,11 @@ class MainApp:
         self.times_button.pack(fill = tk.X)
         self.divide_button = tk.Button(master = self.button_frame, text = "/")
         self.divide_button.pack(fill = tk.X)
-        #equals_button = tk.Button(master = button_frame, text = "=", command = lambda: equals_method(total, curentVal))
 
-        self.clear_button = tk.Button(master = self.button_frame2, text = "C", command = calcValues.clearVals)
+        self.clear_button = tk.Button(master = self.button_frame2, text = "C", command = lambda: calcValues.clearVals(self))
         self.clear_button.pack()
+        self.equals_button = tk.Button(master = self.button_frame2, text = "=", command = lambda: calcValues.equalsMethod(self))
+        self.equals_button.pack()
 
         self.top_frame.pack(side = tk.TOP)
         self.button_frame.pack(side = tk.RIGHT)
@@ -47,36 +89,14 @@ class MainApp:
 
         #self.user_entry.config(validate = "key", validatecommand = (reg, '%P'))
 
-
-
-
-    #adds two integer values
-    def add_method(self, calcValues):
-
-        calcValues.currentVal = self.get_entry_value()   
-        calcValues.runningTotal = calcValues.runningTotal + calcValues.currentVal
-        print("The current value in entry is: " + str(calcValues.currentVal))
-        print("The current value in total is: " + str(calcValues.runningTotal))
-        
-        self.user_entry.delete(0, tk.END)
-        self.user_entry.insert(0, str(calcValues.runningTotal))
-        self.user_entry.insert(tk.END, " + ")
-        
-
-    def get_entry_value(self):
-        parsed_input = parse_input(self.user_entry.get())
-        print("Parsed input: " + parsed_input[-1])
-        return int(parsed_input[-1])
-    
-
-
     def entry_input_callback(self, input):
         
-        split_input = parse_input(input)
         print("Raw input: " + input)
-        print("Last key input: " + split_input[-1])
+        print("Last key input: " + input)
         
-        if split_input[-1].isdigit():
+        if input.isdigit():
+            return True
+        elif input == "":
             return True
         else:
             return False
