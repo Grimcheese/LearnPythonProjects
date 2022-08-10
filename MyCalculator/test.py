ada3da883c3c65337f8e290c27ff75d221451109
Grimcheese <albychawula@gmail.com>
2022-08-10 14:26:52 +0800

    Renamed main.py to mycalc.py. Ran basic tests for StringMath string building and it seems to be working as expected

--- MyCalculator/test.py
+++ MyCalculator/test.py
@@ -1,12 +1,21 @@
 #Unit Testing for MyCalculator
 
-import main
+from mycalc import *
 
 def run_test():
     StringMath_test()
 
 def StringMath_test():
     sm_test = StringMath()
+    if StringMath.validate_operator("5"):
+        print("VALID OPERATOR")
+    else:
+        print("INVALID OPERATOR")
+    sm_test.stringmath_string()
+    sm_test.append_string("+")
+    sm_test.stringmath_string()
+    sm_test.append_string("5")
+    sm_test.append_string("+")
     sm_test.stringmath_string()
 
 run_test()
